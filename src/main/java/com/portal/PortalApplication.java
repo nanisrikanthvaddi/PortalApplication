a8b5228640609d11cae08ec200211c4565258509
Vaddi <srikanth.vaddi@accenture.com>
2021-05-08 01:54:22 +0800

    login page Integration

--- src/main/java/com/portal/PortalApplication.java
+++ src/main/java/com/portal/PortalApplication.java
@@ -1,14 +1,14 @@
 package com.portal;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//http://www.thymeleaf.org/doc/articles/layouts.html
 @SpringBootApplication
 public class PortalApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PortalApplication.class, args);
-    }
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(PortalApplication.class, args);
+	}
 
 }
